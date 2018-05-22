f7f2344aee76810f62d806461daaf957c2b518dd
Unknown <antoine-janvrot@hotmail.fr>
2018-05-22 11:53:08 +0200

    Angular next

--- Angular/mon-projet-angular/src/app/app.component.ts
+++ Angular/mon-projet-angular/src/app/app.component.ts
@@ -1,45 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Observable, interval, Subscription} from 'rxjs';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
+export class AppComponent implements OnInit, OnDestroy {
 
-export class AppComponent {
+  secondes: number;
+  counterSubscription: Subscription;
 
-	isAuth = false;
-	lastUpdate = new Promise((resolve, reject) => {
-		const date = new Date();
-		setTimeout(
-			() => {
-				resolve(date);
-			}, 2000
-			);
-	});
-	appareils = [
-	{
-		name: 'Machine à laver',
-		status: 'éteint'
-	},
-	{
-		name: 'Frigo',
-		status: 'allumé'
-	},
-	{
-		name: 'Ordinateur',
-		status: 'éteint'
-	}
-	];
+  ngOnInit() {
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
 
-	constructor() {
-		setTimeout(
-			() => {
-				this.isAuth = true;
-			}, 4000
-			);
-	}
-	onAllumer() {
-		console.log('On allume tout !');
-	}
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
--- Angular/mon-projet-angular/src/app/app.module.ts
+++ Angular/mon-projet-angular/src/app/app.module.ts
@@ -1,22 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { UserService } from './services/user.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
+
+const appRoutes: Routes = [
+{ path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+{ path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+{ path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
+{ path: 'auth', component: AuthComponent },
+{ path: 'users', component: UserListComponent },
+{ path: 'new-user', component: NewUserComponent },
+{ path: '', component: AppareilViewComponent },
+{ path: 'not-found', component: FourOhFourComponent },
+{ path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MonPremierComponent,
-    AppareilComponent
+  AppComponent,
+  MonPremierComponent,
+  AppareilComponent,
+  AuthComponent,
+  AppareilViewComponent,
+  SingleAppareilComponent,
+  FourOhFourComponent,
+  EditAppareilComponent,
+  UserListComponent,
+  NewUserComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+  BrowserModule,
+  FormsModule,
+  ReactiveFormsModule,
+  RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+  AppareilService,
+  AuthService,
+  AuthGuard,
+  UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
